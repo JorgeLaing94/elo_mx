df1af4da66280e7563c9c18bc4db354ac1b5e83c
Jorge Laing Bernal <jorgelaing@Jorges-MacBook-Pro.local>
2018-02-10 20:35:24 -0800

    updated get_data.py to python 3 and wrote elo_full to analyze all seasons.

---
+++ elo_full.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+First pass at calculating elo scores for each time throughout the league.
+
+@author: jlain
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import plotly.offline as py
+import plotly.graph_objs as go
+
+df = pd.DataFrame.from_csv('full_data.csv')
+
+names = df['eql'].unique()
+keys = dict(zip(names,range(0, len(names))))
+elo_scores = pd.DataFrame(columns=['ds', 'team', 't', 'j', 'elo'])
+
+
+for name in names:
+    elo_scores = elo_scores.append(
+        [{'ds':None, 'team':name, 't':None, 'j':None, 'elo':1500}],
+         ignore_index=True
+        )
+
+def exp_result(r1, r2):
+    return 1.0 / (1 + 10**((r2-r1) / 400))
+
+k = 20
+
+def transform_j(j):
+    if (j == 9999):
+        return 18
+    if (j == 99999):
+        return 19
+    if (j== 999999):
+        return 20
+    return j
+
+
+
+for i in range(len(df)):
+    t, j, eql, eqv, gol, gov, date = df.iloc[i]
+    j = transform_j(j)
+    result = 0.5
+    if (gol > gov):
+        result = 1
+    if (gol < gov):
+        result = 0
+
+    elo_eql = elo_scores[elo_scores['team'] == eql].tail(1)['elo'].iloc[0]
+    elo_eqv = elo_scores[elo_scores['team'] == eqv].tail(1)['elo'].iloc[0]
+
+    expl = exp_result(elo_eql, elo_eqv)
+
+    elo_eql = elo_eql + k * (result - expl)
+    elo_eqv = elo_eqv + k * (expl - result)
+
+    elo_scores.loc[len(elo_scores)] =\
+        {'ds':date, 'team':eql, 't':t, 'j':j, 'elo':elo_eql}
+    elo_scores.loc[len(elo_scores)] =\
+        {'ds':date, 'team':eqv, 't':t, 'j':j, 'elo':elo_eqv}
+
+#PLOT
+title = 'Elo Rankings - Liga MX'
+traces = []
+
+for team in names:
+    to_plot = elo_scores[elo_scores['ds'].notnull()].query("team == '{0}'"\
+        .format(team))
+    traces.append(go.Scatter(
+        x=list((to_plot['ds'])),
+        y=list(to_plot['elo']),
+        mode='lines',
+        connectgaps=False,
+        name=team,
+    ))
+
+layout = dict(title = title,
+              xaxis = dict(title = 'Date'),
+              yaxis = dict(title = 'Elo Score'),
+              )
+
+len(traces)
+fig = dict(data=traces, layout=layout)
+py.iplot(fig, filename='styled-line')
--- get_data.py
+++ get_data.py
@@ -13,34 +13,35 @@ import numpy as np
 import unicodedata as uni
 import dateparser
 
-match_days = range(1, 21) + [88, 9999, 99999, 999999]
-seasons = range(1, 32) + [55, 57, 66, 70, 80, 82, 92, 96, 105, 110, 120]
+match_days = list(range(1, 21)) + [88, 9999, 99999, 999999]
+seasons = list(range(1, 32)) +\
+[55, 57, 66, 70, 80, 82, 92, 96, 105, 110, 120, 123, 132]
 # Season 24 and below should also have matchday 88
 # Some seasons have matchdays 9999991 and 9999992 (Campeon de campeones)
-# Season 16 and below have 
+# Season 16 and below have
 
 data = []
 
 for s in seasons:
-    print "geting season {0}...".format(s)
+    print("geting season {0}...".format(s))
     for i in match_days:
-        print "  ...match day {0}".format(i)
+        print("  ...match day {0}".format(i))
         # Each match day is stored in a different url, so we need to make one
         # request for each match_day.
         url = "http://www.vivoelfutbol.com.mx/jornada.php?to=1&te={0}&jo={1}"\
         .format(s, i)
         page = requests.get(url)
         tree = html.fromstring(page.content)
-        # elements are games and dates        
+        # elements are games and dates
         elements = tree.xpath('/html//div[@class="calendario"]/div[@class="det" or @class="tif"]')
-        d = ""        
-        
+        d = ""
+
         for el in elements:
             if (el.attrib['class'] == 'tif'):
                 # get date
                 d =  uni.normalize('NFKD', el.text)\
                      .encode('ascii', 'ignore').strip()
-                d = d.split(" ", 1)[1]
+                d = str(d).split(" ", 1)[1]
                 # parse and format
                 d = dateparser.parse(d)
                 d = d.strftime("%Y-%m-%d")
@@ -58,7 +59,7 @@ for s in seasons:
                         except ValueError:
                             print("Game has not happened yet!")
                             break
-                
-columns = ['t', 'j', 'eql', 'eqv', 'gol', 'gov']
+
+columns = ['t', 'j', 'eql', 'eqv', 'gol', 'gov', 'date']
 df = pd.DataFrame.from_records(data, columns=columns)
 df.to_csv('full_data.csv')
